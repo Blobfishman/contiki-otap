87ede80ec2e8714515763e8374fb763b616e14d7
Blobfishman <kreienbrink@lp-kb.eu>
2020-07-15 16:20:42 +0200

    fix memory error

--- diff/node/updater-test.c
+++ diff/node/updater-test.c
@@ -5,9 +5,9 @@
 // #include "compression.h"
 // #include "updater.h"
 // test files
-#include "../package-creator/test-files/patch-short.h"
+#include "../package-creator/test-files/patch-long.h"
 #include "../package-creator/test-files/test.h"
-#include "../package-creator/test-files/test-short-compressed.h"
+// #include "../package-creator/test-files/test-long-compressed.h"
 #include "bspatch.h"
 #include "fastlz.h"
 #define LOG_MODULE "App"
@@ -24,20 +24,23 @@ PROCESS_THREAD(udp_server_process, ev, data) {
   LOG_INFO("old:\n");
   LOG_INFO((char *)test_files_test);
   LOG_INFO("\n");
-  LOG_INFO("start decompressing");
-  fastlz2_decompress(test_files_test_short_compressed, test_files_test_short_compressed_len, buffer, BUFFER_SIZE);
-  LOG_INFO("finished decompressing");
-  // fastlz2_decompress(test_files_patch_short, test_files_patch_short_len, buffer, BUFFER_SIZE);
-  LOG_INFO("start bsdiff");
-  bspatch(test_files_test, test_files_test_len, buffer, BUFFER_SIZE, fold_buf, BUFFER_SIZE);
-  LOG_INFO("finished bsdiff");
+
+  LOG_INFO("start decompressing\n");
+  // fastlz2_decompress(test_files_test_long_compressed, test_files_test_long_compressed_len, buffer, BUFFER_SIZE);
+  fastlz2_decompress(test_files_patch_long, test_files_patch_long_len, buffer, BUFFER_SIZE);
+  LOG_INFO("finished decompressing\n");
+  
   LOG_INFO("buffer:\n");
   LOG_INFO((char *)BUFFER_SIZE);
   LOG_INFO("\n");
+
+  LOG_INFO("start bsdiff\n");
+  bspatch(test_files_test, test_files_test_len, buffer, BUFFER_SIZE, fold_buf, BUFFER_SIZE);
+  LOG_INFO("finished bsdiff\n");
+  
   LOG_INFO("new:\n");
-  // LOG_INFO((char *)fold_buf);
+  LOG_INFO((char *)fold_buf);
   LOG_INFO("\n");
-  //   end_update(updater);
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
