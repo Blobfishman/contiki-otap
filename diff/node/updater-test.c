abdfc7ff4a33c061bc9a56179eba8392a02f429d
Blobfishman <kreienbrink@lp-kb.eu>
2020-07-15 11:47:43 +0200

    Change to new interface

--- diff/node/updater-test.c
+++ diff/node/updater-test.c
@@ -1,35 +1,42 @@
-#include "compression.h"
 #include "contiki.h"
 #include "sys/log.h"
-#include "updater.h"
 #include <stdio.h>
+
+// #include "compression.h"
+// #include "updater.h"
+// test files
+#include "../package-creator/test-files/patch-short.h"
+#include "../package-creator/test-files/test.h"
+#include "../package-creator/test-files/test-short-compressed.h"
+#include "bspatch.h"
+#include "fastlz.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
+#define BUFFER_SIZE 1500
 PROCESS(udp_server_process, "updater test");
 AUTOSTART_PROCESSES(&udp_server_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_server_process, ev, data) {
   PROCESS_BEGIN();
-  size_t i = 0;
-  for (; i < 20; i++) {
-    LOG_INFO("updated App");
-  }
-  // struct Updater updater = init_update();
-  unsigned char file[1000];
-  int old_size = 0;
-  int patch = fsd_open(file, 1000);
-  int fold = fsd_open(file, 1000);
-  int target = fsd_open(file, 1000);
-
-  printf("This is a output which is big, you know!");
-
-  fastlz_decompress(patch, old_size, target, 1000);
-
-  bspatch(fold, old_size, target, patch);
-  patch = fsd_close(patch);
-  fold = fsd_close(fold);
-  target = fsd_close(target);
+  unsigned char fold_buf[BUFFER_SIZE];
+  unsigned char buffer[BUFFER_SIZE];
+  LOG_INFO("old:\n");
+  LOG_INFO((char *)test_files_test);
+  LOG_INFO("\n");
+  LOG_INFO("start decompressing");
+  fastlz2_decompress(test_files_test_short_compressed, test_files_test_short_compressed_len, buffer, BUFFER_SIZE);
+  LOG_INFO("finished decompressing");
+  // fastlz2_decompress(test_files_patch_short, test_files_patch_short_len, buffer, BUFFER_SIZE);
+  LOG_INFO("start bsdiff");
+  bspatch(test_files_test, test_files_test_len, buffer, BUFFER_SIZE, fold_buf, BUFFER_SIZE);
+  LOG_INFO("finished bsdiff");
+  LOG_INFO("buffer:\n");
+  LOG_INFO((char *)BUFFER_SIZE);
+  LOG_INFO("\n");
+  LOG_INFO("new:\n");
+  // LOG_INFO((char *)fold_buf);
+  LOG_INFO("\n");
   //   end_update(updater);
   PROCESS_END();
 }
