91fc267b8d71a5fd377dc087a59b0974583d8567
Philipp <philippschlinge@protonmail.com>
2020-07-16 12:34:15 +0200

    myrpl

---
+++ myrpl/skript.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import matplotlib
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
+import re
+import sys
+import numpy as np
+
+server = 0
+dictionary_all = {}
+dictionary_times = {}
+dictionary_energy = {}
+
+def plot():
+    
+    cpu_on = []
+    cpu_lpm = []
+    cpu_deep_lpm = []
+    radio_listen = []
+    radio_transmit = []
+    radio_off = []
+
+    for k,v in dictionary_energy.items():
+        if int(k) == (server - 1):
+            continue
+        
+        tmp = v[-1] 
+        cpu_on.append(tmp[0])
+        cpu_lpm.append(tmp[1])
+        cpu_deep_lpm.append(tmp[2])
+        radio_listen.append(tmp[3])
+        radio_transmit.append(tmp[4])
+        radio_off.append(tmp[5])
+
+    fig = plt.figure(constrained_layout=True)
+    gs = gridspec.GridSpec(6,3, figure= fig)
+    
+    ax1_1 = fig.add_subplot(gs[0,0])
+    ax1_1.bar(np.arange(len(cpu_on)),cpu_on, align='center')
+    ax1_1.set_title('CPU ON')
+
+    ax1_2 = fig.add_subplot(gs[0,1])
+    ax1_2.bar(np.arange(len(cpu_lpm)),cpu_lpm, align='center')
+    ax1_2.set_title('CPU LPM')
+    
+    ax1_3 = fig.add_subplot(gs[0,2])
+    ax1_3.bar(np.arange(len(cpu_deep_lpm)),cpu_deep_lpm, align='center')
+    ax1_3.set_title('CPU DEEP LPM')
+    
+    ax2_1 = fig.add_subplot(gs[1,0])
+    ax2_1.bar(np.arange(len(radio_listen)),radio_listen, align='center')
+    ax2_1.set_title('RADIO LISTEN')
+    
+    ax2_2 = fig.add_subplot(gs[1,1])
+    ax2_2.bar(np.arange(len(radio_transmit)),radio_transmit, align='center')
+    ax2_2.set_title('RADIO TRANSMIT')
+    
+    ax2_3 = fig.add_subplot(gs[1,2])
+    ax2_3.bar(np.arange(len(radio_off)),radio_off, align='center')
+    ax2_3.set_title('RADIO OFF')
+    
+    ax_box = fig.add_subplot(gs[2:,:])
+    ax_box.boxplot(dictionary_times.values())
+    ax_box.set_title('RTTs')
+    
+    plt.show()
+
+
+def parser(logfile, server, node_number):
+    global dictionary_all
+    global dictionary_times
+    global dictionary_energy
+
+    dictionary_times = {new_list: [] for new_list in range(node_number)}
+    dictionary_energy = {new_list: [] for new_list in range(node_number)}
+    dictionary_all = {new_list: [] for new_list in range(node_number)}
+    
+    cpu_time_tmp = 0
+    data_time_tmp = 0
+    
+    app_regex = r"\[INFO:\s+App\s+\]" 
+    cpu_sending_regex = r'Sending CPU'
+    data_sending_regex = r'Sending [^(CPU)]'
+    cpu_received_regex = r"Received response 'CPU"
+    data_received_regex = r"Received response '[^(CPU)]"
+    
+    logfile = open(logfile, 'r')
+    
+    # Traffic Infos der APPs aus Logfile filtern, andere Logs z.B. MAIN SKY ... 
+    # werden ignoriert
+    traffic_all = logfile.readlines()
+    traffic_data = [line for line in traffic_all if re.search(app_regex, line) is not None]
+    
+    # nach Node id filtern 
+    for idx in range(node_number):
+        id_regex = 'ID:' + str(idx + 1)
+        traffic = [line for line in traffic_data if re.search(id_regex, line) is not None]
+        dictionary_all[idx].extend(traffic)
+
+    # Time deltas der einzelnen Pakete errechnen
+    for k,v in dictionary_all.items():
+        data_send = False
+        cpu_send = False
+        # server ignorieren
+        if int(k) == (server - 1):
+            continue
+        for i in v:
+            if re.search(data_sending_regex, i) is not None and data_send == False:
+                x = re.findall(r'\d+', i)
+                data_send = True
+                data_time_tmp = int(x[0])
+            
+            elif re.search(cpu_sending_regex, i) is not None and cpu_send == False:
+                x = re.findall(r'\d+', i)
+                cpu_send = True
+                cpu_time_tmp = int(x[0])
+
+            elif re.search(data_received_regex, i) is not None and data_send == True:
+                x = re.findall(r'\d+', i)
+                data_send = False
+                delta_time = int(x[0]) - data_time_tmp
+                dictionary_times[k].append(delta_time)
+
+            elif re.search(cpu_received_regex, i) is not None and cpu_send == True:
+                x = re.findall(r'\d+', i)
+                cpu_send = False
+                delta_cpu = int(x[0]) - cpu_time_tmp
+                dictionary_times[k].append(delta_cpu)
+
+                y = re.split("'", i)
+                z = re.findall(r"\d+", y[1])
+                z = [int(i) for i in z]
+                w = tuple(z)
+                dictionary_energy[k].append(w)
+    
+    logfile.close()
+
+def main(args):
+    global server
+    if len(sys.argv) != 4:
+        print("Error: use ./skript.py <Logfile> <Server_ID> <Number_of_Nodes>")
+    logfile = str(sys.argv[1])
+    server = int(sys.argv[2])
+    node_number = int(sys.argv[3])
+    parser(logfile,server,node_number)
+    plot()
+
+
+if __name__ == '__main__':
+    main(sys.argv) 
---
+++ myrpl/udp-client-pressure.c
@@ -0,0 +1,211 @@
+#include "contiki.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/netstack.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "sys/energest.h"
+#include "net/nbr-table.h"
+#include "net/routing/rpl-lite/rpl-neighbor.h"
+#include "net/link-stats.h"
+#include "sys/log.h"
+
+#include "myrpl_conf.h"
+
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#define WITH_SERVER_REPLY 1
+#define UDP_CLIENT_PORT 8765
+#define UDP_SERVER_PORT 5678
+
+#define UDP_RPL_CLIENT_PORT 4321
+#define UDP_RPL_SERVER_PORT 1234
+
+static struct ctimer timer_energy;
+static struct ctimer timer_app;
+static struct ctimer timer_transmit;
+
+static struct simple_udp_connection udp_conn;
+static struct simple_udp_connection udp_tx_rpl_conn;
+static struct simple_udp_connection udp_rx_rpl_conn;
+
+uip_ipaddr_t dest_ipaddr;
+uip_ipaddr_t *root;
+
+NBR_TABLE_GLOBAL(struct nbr_stats, transmit_rates_table);
+/*---------------------------------------------------------------------------*/
+static unsigned long to_seconds(uint64_t time) {
+    return (unsigned long)(time / ENERGEST_SECOND);
+}
+/*---------------------------------------------------------------------------*/
+void energy_callback(void *ptr) {
+    static char str[128];
+    /* rearm the ctimer */
+    ctimer_reset(&timer_energy);
+
+    if (NETSTACK_ROUTING.node_is_reachable() &&
+        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+        /* Send to DAG root */
+        energest_flush();
+
+        snprintf(str, sizeof(str),
+                 "CPU=ON:%4lus LPM:%4lus, DEEP_LPM:%4lus; "
+                 "RADIO=LISTEN:%4lus,TRANSMIT:%4lus,OFF:%4lus",
+                 to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
+                 to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
+                 to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
+                 to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
+                 to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+                 to_seconds(ENERGEST_GET_TOTAL_TIME() -
+                            energest_type_time(ENERGEST_TYPE_TRANSMIT) -
+                            energest_type_time(ENERGEST_TYPE_LISTEN)));
+
+        simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+        LOG_INFO("Sending ");
+        LOG_INFO_(str);
+        LOG_INFO_(" to ");
+        LOG_INFO_6ADDR(&dest_ipaddr);
+        LOG_INFO_("\n");
+
+    } else {
+        LOG_INFO("Not reachable yet\n");
+    }
+}
+
+void app_callback(void *ptr) {
+    static char str[32];
+    static int pressure;
+    /* rearm the ctimer */
+    ctimer_reset(&timer_app);
+    pressure = (random_rand() % 101) + 1000;
+    if (NETSTACK_ROUTING.node_is_reachable() &&
+        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+        /* Send to DAG root */
+        LOG_INFO("Sending pressure: %d hPa request to ", pressure);
+        LOG_INFO_6ADDR(&dest_ipaddr);
+        LOG_INFO_("\n");
+      snprintf(str, sizeof(str), "pressure: %d hPa", pressure);
+      simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+    } else {
+        LOG_INFO("Not reachable yet\n");
+    }
+}
+
+void transmit_rate_callback(void *ptr){
+    ctimer_reset(&timer_transmit);
+    static char str[] = "transmit";
+    struct nbr_stats* stats;
+    /* uint16_t tmp = 0; */
+
+    rpl_nbr_t *nbr = nbr_table_head(rpl_neighbors);
+    while (nbr != NULL){
+        stats = nbr_table_get_from_lladdr(transmit_rates_table, rpl_neighbor_get_lladdr(nbr));
+        if(stats == NULL){
+            stats = nbr_table_add_lladdr(transmit_rates_table, rpl_neighbor_get_lladdr(nbr), NBR_TABLE_REASON_UNDEFINED, NULL);
+        }
+        /* transmit_rate = &tmp; */
+        uip_ipaddr_t *receiver_ipaddr =  rpl_neighbor_get_ipaddr(nbr);
+        LOG_INFO("Transmitrate: %ds from ", stats->tx_time);
+        LOG_INFO_6ADDR(receiver_ipaddr);
+        LOG_INFO_("\n");
+        
+        const struct link_stats *link;
+        link = link_stats_from_lladdr(rpl_neighbor_get_lladdr(nbr));
+        LOG_INFO("etx: %d, rssi: %d from ", link->etx, link->rssi);
+        LOG_INFO_6ADDR(receiver_ipaddr);
+        LOG_INFO_("\n");
+
+        simple_udp_sendto(&udp_tx_rpl_conn, str, strlen(str), receiver_ipaddr);
+        nbr = nbr_table_next(rpl_neighbors, nbr);
+
+    }
+    
+}
+static void udp_rpl_tx_callback(struct simple_udp_connection *c,
+                            const uip_ipaddr_t *sender_addr,
+                            uint16_t sender_port,
+                            const uip_ipaddr_t *receiver_addr,
+                            uint16_t receiver_port, const uint8_t *data,
+                            uint16_t datalen) {
+
+    /* LOG_INFO("Received trasmitrate '%.*s' from ", datalen, (char *)data); */
+    /* LOG_INFO_6ADDR(sender_addr); */
+    /* LOG_INFO_("\n"); */
+    unsigned int data_num = 0;
+    unsigned int base = 1;
+    int i;
+    for(i = 0; i < datalen; i++){
+        data_num *= base;
+        data_num += (*data - 48);
+        base *= 10;
+        data++;
+    }
+
+    struct nbr_stats* stats;
+    rpl_nbr_t *nbr = rpl_neighbor_get_from_ipaddr((uip_ipaddr_t*)sender_addr);
+    stats = nbr_table_get_from_lladdr(transmit_rates_table,rpl_neighbor_get_lladdr(nbr));
+    stats->tx_time = data_num;
+    /* LOG_INFO("Transmitrate: %d from ", stats->tx_time); */
+    /* LOG_INFO_6ADDR(sender_addr); */
+    /* LOG_INFO_("\n"); */
+}
+
+static void udp_rpl_rx_callback(struct simple_udp_connection *c,
+                            const uip_ipaddr_t *sender_addr,
+                            uint16_t sender_port,
+                            const uip_ipaddr_t *receiver_addr,
+                            uint16_t receiver_port, const uint8_t *data,
+                            uint16_t datalen) {
+
+    unsigned int transmit_rate = 0;
+    static char str[32];
+    /* LOG_INFO("Transmitrate request from: "); */
+    /* LOG_INFO_6ADDR(sender_addr); */
+    /* LOG_INFO_("\n"); */
+
+    energest_flush();
+    unsigned int tranmit_time = (unsigned int)to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT));
+    /* unsigned long total_time = to_seconds(ENERGEST_GET_TOTAL_TIME()); */
+
+    transmit_rate = tranmit_time; 
+    snprintf(str, sizeof(str), "%d", transmit_rate);
+    simple_udp_sendto(&udp_rx_rpl_conn, str, strlen(str), sender_addr);
+
+}
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client");
+AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void udp_rx_callback(struct simple_udp_connection *c,
+                            const uip_ipaddr_t *sender_addr,
+                            uint16_t sender_port,
+                            const uip_ipaddr_t *receiver_addr,
+                            uint16_t receiver_port, const uint8_t *data,
+                            uint16_t datalen) {
+    LOG_INFO("Received response '%.*s' from ", datalen, (char *)data);
+    LOG_INFO_6ADDR(sender_addr);
+#if LLSEC802154_CONF_ENABLED
+    LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+#endif
+    LOG_INFO_("\n");
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data) {
+    PROCESS_BEGIN();
+
+    /* Initialize UDP connection */
+    simple_udp_register(&udp_tx_rpl_conn, UDP_RPL_CLIENT_PORT, NULL, UDP_RPL_SERVER_PORT,
+                        udp_rpl_tx_callback);
+    simple_udp_register(&udp_rx_rpl_conn, UDP_RPL_SERVER_PORT, NULL, UDP_RPL_CLIENT_PORT,
+                        udp_rpl_rx_callback);
+    simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT,
+                        udp_rx_callback);
+    
+    nbr_table_register(transmit_rates_table, NULL);
+
+    ctimer_set(&timer_energy, 10 * CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)), energy_callback, NULL);
+    ctimer_set(&timer_app, 3 * CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)), app_callback, NULL);
+    ctimer_set(&timer_transmit, 5 * CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)), transmit_rate_callback, NULL);
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
